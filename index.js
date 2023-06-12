247e0e119d075ae0656ef481af942d594f20ac9c
Nawal Jama <nawaljng@gmail.com>
2023-06-12 07:39:14 -0400

    added api key, basic html and js code to define things

---
+++ index.js
@@ -0,0 +1,6 @@
+const apiKey = "ad992c8e60294bff45aa9cead11cdc53"
+const locationElement = document.querySelector('.location');
+const temperatureElement = document.querySelector('.temperature');
+const descriptionElement = document.querySelector('.description');
+const weatherIconElement = document.querySelector('.weather-icon');
+const forecastListElement = document.querySelector('.forecast-list');
